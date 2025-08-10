dc2ce8aaee82f847aae7e187e36215c5c23e4095
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2025-08-10 14:02:14 -0400

    added switch

--- calculator.java
+++ calculator.java
@@ -9,6 +9,22 @@ public class calculator {
         n1 = sc.nextInt();
         System.out.print("Enter second number ; ");
         n2 = sc.nextInt();
+
+        int result = 0;
+        switch(operator) {
+            case 1:
+                result = n1 + n2;
+                break;
+            case 2:
+            result = n1 - n2;
+                break;
+            case 3:
+            result = n1 * n2;
+                break;
+            case 4:
+            result = n1 / n2;
+                break;
+        }
     }
     
 }
