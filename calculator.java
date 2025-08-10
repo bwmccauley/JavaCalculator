192b5860f0c51e66febfde32be20843e8f1ecb86
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2025-08-10 14:11:43 -0400

    fixed errors

--- calculator.java
+++ calculator.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class calculator {
     public static void main (String [] arguments) {
         int operator, n1, n2;
         System.out.println("1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide");
         System.out.print("Choose Operator: ");
         Scanner sc = new Scanner(System.in);
-        operator = sc.extInt();
+        operator = sc.nextInt();
         System.out.print("Enter first number ; ");
         n1 = sc.nextInt();
         System.out.print("Enter second number ; ");
