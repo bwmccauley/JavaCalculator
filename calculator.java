93cf2ee357144768ac3080c606f38ddea85871d8
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2025-08-10 13:18:21 -0400

    added more logic

--- calculator.java
+++ calculator.java
@@ -2,7 +2,13 @@ public class calculator {
     public static void main (String [] arguments) {
         int operator, n1, n2;
         System.out.println("1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide");
-        
+        System.out.print("Choose Operator: ");
+        Scanner sc = new Scanner(System.in);
+        operator = sc.extInt();
+        System.out.print("Enter first number ; ");
+        n1 = sc.nextInt();
+        System.out.print("Enter second number ; ");
+        n2 = sc.nextInt();
     }
     
 }
